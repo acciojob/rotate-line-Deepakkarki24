a3c3c44d1735d04f941d9c60e57ea9600505ec3a
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 07:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ let line = document.getElementById("line");
 let angle = 0;
 setInterval(() => {
   angle += 2;
+	line.style.position = 'absolute'
   line.style.transform = `rotate(${angle}deg)`;
 }, 20);
